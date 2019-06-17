fd1e0ab75db4d1cc1d5ac666fe7040621fb5e8ea
Monique Wagner <mmoniqueww@gmail.com>
2019-06-17 10:26:54 -0300

    Tabela de prédioxrequisito começada

---
+++ app/Http/Controllers/TabelaController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Predio;
+use App\Requisito;
+use App\Tabela;
+use Illuminate\Http\Request;
+
+class TabelaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = array();
+        $data['predio'] = Predio::all();
+        $data['requisito'] = Requisito::all();
+        return view('tabela', compact ('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tabela');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Predio::findOrFail($id);
+        return Requisito::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
